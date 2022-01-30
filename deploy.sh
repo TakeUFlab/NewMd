2b1a427d2647aee09a71ac56cf0ec2aa52e8cdcd
LAZPbanahaker(Andy) <pn0818x@outlook.com>
2022-01-31 07:50:43 +0800

    deploy prepare

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env sh
+set -e
 
-echo "hello, world"
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
+cd -
