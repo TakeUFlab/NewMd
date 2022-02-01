8c11584e9a2e4cd263b9d920ad8c6db29e1e9aaf
LAZPbanahaker(Andy) <pn0818x@outlook.com>
2022-02-01 22:31:14 +0800

    AUTOcommit

---
+++ AUTOcommit.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+#auto commit bash code
+git status
+git add .
+git commit -m "AUTOcommit"
+
+git push
+#AUTOcommit only use in unimportent commit, please commit with desciption every time
